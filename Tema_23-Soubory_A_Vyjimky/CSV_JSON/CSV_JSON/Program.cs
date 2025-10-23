26022ab452b9a9c78fb220da036ba1e9a970aacb
Fas1kD <131455483+Fas1kD@users.noreply.github.com>
2025-10-23 12:45:41 +0200

    JSON a CVS

---
+++ Tema_23-Soubory_A_Vyjimky/CSV_JSON/CSV_JSON/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace CSV_JSON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cesta = "lide.csv";  // cesta k souboru je "lide.csv"
+            string[] radky = File.ReadAllLines(cesta);  //ReadAllLines - vypiš všechny řádky z cesta
+
+            foreach(var radek in radky)     //pro kazdy radek v radky vypis ten jeden radek a pokracuj dokud nebudes u posledniho radku
+            {
+                Console.WriteLine(radek);
+            }
+
+            var delkaRadku = radky.Length - 1;      // proměná z radky.Lenght - 1 --->>> počet řádků minus první řádek
+
+            //array pro kazde jmeno, prijmeni,...
+            string[] jmeno = new string[delkaRadku];
+            string[] prijmeni = new string[delkaRadku];
+            string[] misto = new string[delkaRadku];
+            int[] rok = new int[delkaRadku];
+
+            string[] aktaulniRadek = radky[1].Split(";"); //rozdeleni prvního (druhého) radku, kde je ; se dá mezera
+            Console.WriteLine(aktaulniRadek[0]);    //[0] - číslo sloupce - 0-jmeno, 1-prijmeni, 2-misto, 3-rok
+
+
+            //vypíše všechno z jednoho sloupce
+            int i = 1;
+            string[] jmeno2 = radky[i].Split(";");
+            foreach (var cokoliv in radky)
+            {
+                if (i == radky.Length)
+                {
+                    Console.WriteLine("konec");
+                    break;
+                }
+                else
+                {
+                    jmeno2 = radky[i].Split(";");
+                    Console.WriteLine(jmeno2[0]);
+                    i++;
+
+                }
+                
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ Tema_23-Soubory_A_Vyjimky/CSV_JSON_ten_funkcni/CSV_JSON_ten_funkcni/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Text.Json;
+
+namespace CSV_JSON_ten_funkcni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //CSV
+            string cesta = "lide.csv";
+            string[] radky = File.ReadAllLines(cesta);
+
+            // jen zkušební, vytiskne celý array
+            //foreach (var radek in radky)
+            //{
+            //    Console.WriteLine(radek);
+            //}
+
+            //// První řádek je hlavička, začneme od indexu 1
+            //for (int i = 1; i < radky.Length; i++)
+            //{
+            //    string[] hodnoty = radky[i].Split(',');
+
+            //    string jmeno = hodnoty[0];
+            //    int vek = int.Parse(hodnoty[1]);
+            //    string mesto = hodnoty[2];
+
+            //    Console.WriteLine($"{jmeno} ({vek}) - {mesto}");
+            //}
+
+            int n = radky.Length;
+            string[] jmena = new string[n];
+            string[] prijmeni = new string[n];
+            string[] mesta = new string[n];
+            int[] rokyNarozeni = new int[n];
+
+            //i je proměná, po pokaždém ciklu se přidá +1, 
+            for (int i = 1; i < n; i++)
+            {
+                string[] aktualniRadek = radky[i].Split(';');
+                jmena[i] = aktualniRadek[0];    //z řádku "i" a sloupce 0 se přidá do array jmena to co je v lide.csv na dané pozici
+                prijmeni[i] = aktualniRadek[1]; //z řádku "i" a sloupce 1 se přidá do array prijmeni to co je v lide.csv na dané pozici
+                mesta[i] = aktualniRadek[2];    //z řádku "i" a sloupce 2 se přidá do array mesta to co je v lide.csv na dané pozici
+                rokyNarozeni[i] = int.Parse(aktualniRadek[3]);  //z řádku "i" a sloupce 3 se přidá do array rokyNarozeni to co je v lide.csv na dané pozici
+            }
+            foreach (var jmeno in jmena)
+            {
+                Console.WriteLine(jmeno);
+            }
+
+            Console.WriteLine("---------------");
+            foreach(var prij in prijmeni)
+            {
+                Console.WriteLine(prij);
+            }
+
+            Console.WriteLine("--------------------");
+            int vyskytBrno = mesta.Count(m => m == "Brno");
+            Console.WriteLine(vyskytBrno);
+
+
+            //JSON
+            Dictionary<string, int> ucastnici = new Dictionary<string, int>()
+            {
+                {"Pavel", 2007 },
+                {"Franta", 2000 },
+                {"Jan", 2006 }
+            };
+
+            string json = JsonSerializer.Serialize(ucastnici);  //serializace do podoby zapisu
+            File.WriteAllText("ucastnici.json", json); //uložit jako ucastnici.json do jsonu. uloží se to do "soubor projektu/bin/Debug/net8.0/ucastnici - otevrit v poznamkach
+
+
+            //Vypsání z uloženého JSONU
+            string nactenyJsonu = File.ReadAllText("ucastnici.json");
+            Console.WriteLine(nactenyJsonu);
+            var nactenyDict = JsonSerializer.Deserialize<Dictionary<string, int>>(nactenyJsonu);
+            Console.WriteLine(nactenyDict["Jan"]);
+
+
+            Console.ReadKey();
+        }
+    }
+}
