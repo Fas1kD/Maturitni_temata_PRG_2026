579485eb8c6b1f9c2d64e21d168217568eaedcdf
Fas1kD <131455483+Fas1kD@users.noreply.github.com>
2025-11-04 11:24:05 +0100

    OOP

---
+++ Tema_17_OOP/Tema_17_OOP/Osoba.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema_17_OOP
+{
+    internal class Osoba
+    // VYTVOŘENÍ NOVÉ CLASS Pravím na projekt - přidat - class - přejmenovat na Osoba.cs
+    {
+        public string Jmeno; // VYTVOŘENÍ ATRIBUTŮ CLASS
+        public int Vek;
+
+        //Konstraktor - "CTOR" + TAB
+        //bude potřeba při vytváření objektu
+        public Osoba(string jmeno, int vek)
+        {
+            Jmeno = jmeno;
+            Vek = vek;
+            List<int> znamky = new();
+        }
+
+
+        //Public - lze měnit zvenčí
+        //Private - malé písmeno a _ před názvem, je chráněná atribut, nelze zvenčí měnit, potom se píše get a set
+
+        public void PredstavSe() // VYTVOŘENÍ METODY CLASS
+        {
+            Console.WriteLine($"Ahoj, jmenuji se {Jmeno} a je mi {Vek} let.");
+        }
+    }
+}
---
+++ Tema_17_OOP/Tema_17_OOP/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Tema_17_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vytvoření class osoba.cs
+            Osoba Osoba_1 = new("Jan", 20); // Osoba - název class, Osoba_1 - název 
+            Osoba_1.PredstavSe(); // Volání metody class - s hodnotami z CTOR
+
+
+            //Toto se potom řeší přes konstruktor (CTOR) v Osoba.cs
+            Osoba_1.Jmeno = "Franta"; // Přiřazení hodnot atributům class
+            Osoba_1.Vek = 25;
+
+            Osoba_1.PredstavSe(); // - volání z Osoba.cs s přiřazenými hodnotami v Program.cs
+        }
+    }
+}
