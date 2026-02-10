7469ad625f6b460455fab1c2726e79ee4c0fef37
Fas1kD <131455483+Fas1kD@users.noreply.github.com>
2026-02-10 11:14:59 +0100

    Add files via upload

---
+++ inkscape/inkscape/rozmery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inkscape
+{
+    internal class rozmery
+    {
+        public double sirka { get; set; } = 120;
+        public double vyska { get; set; } = 60;
+        public double x { get; set; } = 50;
+        public double y { get; set; } = 40;
+    }
+}
